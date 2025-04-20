67f3c392f7b479ac10c6efb86ff06536c27151d2
Mahmoud Ali <147126602+Mahmoud-Ali91@users.noreply.github.com>
2025-04-20 18:32:20 +0200

    Update application.py

--- application.py
+++ application.py
@@ -172,7 +172,7 @@ if remaining > 0:
  break_even_units = round(investment_cap_egp / (remaining * exchange_rate), 0)
 else:
  break_even_units = float('inf')  # not feasible
-    results.append({
+results.append({
             "Target Price (SAR)": price,
             "Fixed Size (ml)": size,
             "Dynamic Cost per ml (EGP)": round(dynamic_cost_egp, 2),
